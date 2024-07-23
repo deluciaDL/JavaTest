8bebdb678c39106117324cced836d389df4dd8d9
Alvaro de Lucia <alvaro.delucia@yapiko.com>
2024-07-23 15:09:15 +0200

    PriceController added on infrastructure layer. Pom file has been updated to add swagger on spring 3

---
+++ src/main/java/com/example/hexagonal/infrastructure/controller/PriceController.java
@@ -0,0 +1,49 @@
+package com.example.hexagonal.infrastructure.controller;
+
+import com.example.hexagonal.application.dto.PriceResponse;
+import com.example.hexagonal.application.service.PriceService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/prices")
+@OpenAPIDefinition(tags = { @Tag(name = "Prices") })
+public class PriceController {
+
+    private final PriceService priceService;
+
+    public PriceController(PriceService priceService) {
+        this.priceService = priceService;
+    }
+
+    @Operation(method = "GET", summary = "Get a price by date, productId and brandId", description = "Fetches the price details for a given date, productId and brandId.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Price found and returned", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PriceResponse.class)) }),
+            @ApiResponse(responseCode = "404", description = "Price not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error") })
+    @GetMapping
+    public ResponseEntity<PriceResponse> getPrice(
+            @Parameter(description = "Price date", example = "2024-07-20:08.00.00") @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd:HH.mm.ss")
+            LocalDateTime date,
+            @Parameter(description = "Product identifier associated to the price", example = "150") @RequestParam("productId")
+            Long productId,
+            @Parameter(description = "Brand identifier associated to the price", example = "1") @RequestParam("brandId")
+            Long brandId) {
+        return ResponseEntity.ok(priceService.getPrice(date, brandId, productId));
+    }
+}
